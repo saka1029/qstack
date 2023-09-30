2923919be209b165500c314cc7023e3231b50dd1
saka1029 <saka1029@gmail.com>
2023-09-30 22:36:51 +0900

    Update TestReader.java

--- src/test/java/saka1029/qstack/TestReader.java
+++ src/test/java/saka1029/qstack/TestReader.java
@@ -10,11 +10,20 @@ import org.junit.Test;
 public class TestReader {
     
     @Test
-    public void testRead() {
+    public void testReadElement() {
         Context c = Context.of(10);
-        Reader reader = Reader.of(c, new StringReader("  1 2 "));
+        Reader reader = Reader.of(c, new StringReader("  1 -2   drop "));
         assertEquals(Int.ONE, reader.read());
-        assertEquals(Int.TWO, reader.read());
+        assertEquals(Int.of(-2), reader.read());
+        assertEquals(Reference.of("drop"), reader.read());
+        assertNull(reader.read());
+    }
+    
+    @Test
+    public void testReadList() {
+        Context c = Context.of(10);
+        Reader reader = Reader.of(c, new StringReader("  (1 -2   drop) "));
+        assertEquals(List.of(Int.ONE, Int.of(-2), Reference.of("drop")), reader.read());
         assertNull(reader.read());
     }
 
