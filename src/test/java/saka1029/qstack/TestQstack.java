0810de848616d2b9b9ace3a085be70ba528c1b21
saka1029 <saka1029@gmail.com>
2023-10-01 17:03:08 +0900

    Create TestQstack.java

---
+++ src/test/java/saka1029/qstack/TestQstack.java
@@ -0,0 +1,21 @@
+package saka1029.qstack;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestQstack {
+
+    @Test
+    public void testFactRecursive() {
+        Context c = Context.of(10);
+        c.run("'(@0 0 <= '(drop 1) '(@0 1 - ! *) if) '! define");
+        assertEquals(c.eval("1"), c.eval("0 !"));
+        assertEquals(c.eval("1"), c.eval("1 !"));
+        assertEquals(c.eval("2"), c.eval("2 !"));
+        assertEquals(c.eval("6"), c.eval("3 !"));
+        assertEquals(c.eval("24"), c.eval("4 !"));
+        assertEquals(c.eval("120"), c.eval("5 !"));
+    }
+
+}
