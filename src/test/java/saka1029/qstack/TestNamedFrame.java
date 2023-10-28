824438e8be6d53e0d1156a7bc74e8db66d188dac
saka1029 <saka1029@gmail.com>
2023-10-28 18:11:14 +0900

    Update TestNamedFrame.java

--- src/test/java/saka1029/qstack/TestNamedFrame.java
+++ src/test/java/saka1029/qstack/TestNamedFrame.java
@@ -8,8 +8,14 @@ public class TestNamedFrame {
 
     @Test
     public void testRead() {
-        Context c = Context.of(80);
+        Context c = Context.of(8);
         assertEquals(c.eval("'(1 1 : A1 A1 +)"), c.eval("'(F 1 n : n n +)"));
+        assertEquals(c.eval("'(1 1 : (1 1 : A11 A1 +))"), c.eval("'(F 1 a : (F 1 n : a n +))"));
+    }
+    
+    @Test
+    public void testFact() {
+        Context c = Context.of(80);
         c.run("'(F 1 n : n 0 <= 1 '(n 1 - self n *) if) 'fact define");
         assertEquals(Int.of(1), c.eval("0 fact"));
         assertEquals(Int.of(1), c.eval("1 fact"));
